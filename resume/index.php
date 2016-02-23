96b7c822759531bd1cc5954e24ee3a11cf16a9d2
minmingsheng <shengminming1990@gmail.com>
2016-02-23 15:54:57 -0500

    add sound add some animation

--- resume/index.php
+++ resume/index.php
@@ -16,35 +16,95 @@
 <html>
 	<head>
 		<title>resume</title>
-	</head>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- for Google -->
+			<meta name="description" content="My name is Minming Sheng, I'm a front-end web developer&designer based in Toronto, Canada." />
+			<meta name="keywords" content="web development, front-end web development, Jason Minming Sheng, toronto, freelance" />
+			<meta name="author" content="Minming Sheng" />
+			<link rel="author" href="https://plus.google.com/u/0/105615322120201556614">
+			<meta name="copyright" content="Minming Sheng, 2016" />
 
+			<!-- for Facebook -->          
+			<meta property="og:title" content="Minming Sheng &mdash; Front-End Web Developer" />
+			<meta property="og:type" content="website" />
+			<meta property="og:image" content="" /> <!-- 1200 x 630 -->
+			<meta property="og:url" content="http://minmingsheng.design/" />
+			<meta property="og:description" content="My name is Minming Sheng, I'm a front-end web developer based in Toronto, Canada." />
+
+			<!-- for Twitter -->          
+			<meta name="twitter:card" content="summary_large_image" />
+			<meta name="twitter:title" content="Minming Sheng &mdash; Front-End Web Developer" />
+			<meta name="twitter:description" content="My name is Darren Hebner, I'm a front-end web developer based in Toronto, Canada." />
+			<meta name="twitter:image:src" content="" /> <!-- 1,024 x 512 -->
+			<meta name="twitter:site" content="@smm_jason">
+		    <meta name="twitter:creator" content="@smm_jason">
+
+		<link rel="apple-touch-icon" sizes="57x57" href="img/favicons/apple-touch-icon-57x57.png">
+		<link rel="apple-touch-icon" sizes="60x60" href="img/favicons/apple-touch-icon-60x60.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="img/favicons/apple-touch-icon-72x72.png">
+		<link rel="apple-touch-icon" sizes="76x76" href="img/favicons/apple-touch-icon-76x76.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="img/favicons/apple-touch-icon-114x114.png">
+		<link rel="apple-touch-icon" sizes="120x120" href="img/favicons/apple-touch-icon-120x120.png">
+		<link rel="apple-touch-icon" sizes="144x144" href="img/favicons/apple-touch-icon-144x144.png">
+		<link rel="apple-touch-icon" sizes="152x152" href="img/favicons/apple-touch-icon-152x152.png">
+		<link rel="apple-touch-icon" sizes="180x180" href="img/favicons/apple-touch-icon-180x180.png">
+		<link rel="icon" type="image/png" href="img/favicons/favicon-32x32.png" sizes="32x32">
+		<link rel="icon" type="image/png" href="img/favicons/android-chrome-192x192.png" sizes="192x192">
+		<link rel="icon" type="image/png" href="img/favicons/favicon-96x96.png" sizes="96x96">
+		<link rel="icon" type="image/png" href="img/favicons/favicon-16x16.png" sizes="16x16">
+		<link rel="manifest" href="img/favicons/manifest.json">
+		<link rel="mask-icon" href="img/favicons/safari-pinned-tab.svg" color="#5bbad5">
+		<meta name="apple-mobile-web-app-title" content="resume">
+		<meta name="application-name" content="resume">
+		<meta name="msapplication-TileColor" content="#da532c">
+		<meta name="msapplication-TileImage" content="/mstile-144x144.png">
+		<meta name="theme-color" content="#ffffff">
+			
+			<!--[if lt IE 9]>
+		        <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		    <![endif]-->
+	</head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 	<link rel="stylesheet" href="css/reset.css">
 	<link rel="stylesheet" href="css/main.css">
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
 	<script type="text/javascript" src="js/main.js"></script>
 	<body>
+		<div class="gray"></div>
+		<div class="showDes">
+			<section>
+				<span>x</span>
+				<b>asdas</b>
+				<div></div>
+<!-- 			<div>asdads</div>
+				<div>asdads</div>
+				<div>asdads</div>
+				<div>asdads</div> -->
+			</section>
+		</div>
 		<div class="container">
 			<div class="wraper">
 				<div class='title pagewidth'>
 					<h1><a href="add.php">MINMING SHENG</a></h1>
 					<h2>Interdisciplinary Designer</h2>
-					<p>Based on Toronto</p>
+					<p>Based in Toronto</p>
 				</div>
 				<div class='education pagewidth'>
-					<div class="banner"><h2>Education</h2></div>
+					<div class="banner changColor"><h2>Education</h2></div>
 					<div class= "edus">
 						<div class="edu">
 							<div>
-								<p><b>POST Graducate Certificate SEP 2015 - APR 2016 </b><br> Sheridan College</p>
+								<p><b>POST Graducate Certificate (SEP 2015 - APR 2016) </b><br> Sheridan College</p>
 							</div>
 							<div>
-								<p><b>Web Design</b></p>
+								<p><b>Interactive Multimedia</b></p>
 							</div>
 						</div>
 						<div class="edu">
 							<div>
-								<p><b>POST-Baccalaureate Certificate SEP 2014 - APR 2015</b> <br> NSCAD University </p>
+								<p><b>POST-Baccalaureate Certificate (SEP 2014 - APR 2015)</b> <br> NSCAD University </p>
 							</div>
 							<div>
 								<p><b>Interdisciplinary Design</b></p>
@@ -52,7 +112,7 @@
 						</div>
 						<div class="edu">
 							<div>
-								<p><b>BACHELOR DEGREE OF ARTS SEP 2010 - JUN 2014</b> <br> Shanghai University</p>
+								<p><b>BACHELOR DEGREE OF ARTS (SEP 2010 - JUN 2014)</b> <br> Shanghai University</p>
 							</div>
 							<div>
 								<p><b>Visual Communication</b></p>
@@ -61,13 +121,13 @@
 					</div>
 				</div>
 				<div class='personalSkill pagewidth'>
-					<div class="banner"><h2>Personal Skill</h2></div>
+					<div class="banner changColor"><h2>Personal Skill</h2></div>
 					<div class="contentWraper">
 						<div class="skillList">
 							<ul>
 							<?php foreach($skills as $key => $values): ?>
 						
-								<li>
+								<li class="eachSkill">
 									<ul class="skillSet" data-id="<?php echo $skills[$key]["id"] ?>">
 										<li><b><?php echo $skills[$key]["name"] ;?></b></li>
 										<li>
@@ -82,12 +142,29 @@
 						</div>
 						<div class="skillInfo">
 							<div class="infoFeed">
+
+								<div class="intro" >
+									<b>Applying: </b>Junior front-end developer or junior ux/ui designer. $24-$30 / hour is my wish. 
+								</div>
+								<div class="intro" >
+									<b>Background: </b>I have a graphic designer background with a little 3D, drawing & photography.
+								</div>
+								<div class="intro" >
+									<b>Current year: </b>I use one year to learning front-end and back-end development as well as ux/ui design.
+								</div>
+								<div class="intro" >
+									<b>Feeling: </b>I know there are a lot still need to learn but I learn fast. 
+								</div>
+								<div class="intro" >
+									<b>Thought: </b>Everything could be fake, but the hand would not, if having a chance to get an interview, I will take my keyboard
+									to show what I can do.
+								</div>
 							</div>
 						</div>
 					</div>
 				</div>
 				<div class='workExperience pagewidth'>
-					<div class="banner"><h2>Work Experience</h2></div>
+					<div class="banner changColor"><h2>Work Experience</h2></div>
 					<div>
 						<p><b> 2013 - 2014	Herun Group Shanghai Development Co., Ltd.  Shanghai</b><br><br>Graphic Designer</p>
 						<p>Took an internship as Graphic Designer to assist in website designing and leaflet designing .</p>
@@ -106,5 +183,7 @@
 				</div>
 			</div>
 		</div>
+		<div class="pause"><i class="fa fa-file-audio-o"></i></div>
+		<audio id="audioElement" src="audio/happy.aac"></audio>
 	</body>
 </html>
--- resume/js/main.js
+++ resume/js/main.js
@@ -1,59 +1,115 @@
 window.addEventListener("load", function(){
-	// config
+	
 
-		document.querySelector(".container").style.width = window.innerWidth+'px';
+	document.querySelector(".container").style.width = window.innerWidth+'px';
 
 
 	var descriptions;
 	var skillSet = document.querySelectorAll(".skillSet");
 	var infoFeed = document.querySelector(".infoFeed");
 	var dataId; // getAttribute from data-id --> is the N.O of skills
-	function loadDoc(a) {/*pass id*/
+	function loadDoc(a, title) {/*pass id*/
 	  var xhttp = new XMLHttpRequest();
 	  xhttp.open("GET", "description.php", true);
 	  xhttp.send();
 	  xhttp.onreadystatechange = function() {
 	    if (xhttp.readyState == 4 && xhttp.status == 200) {
 	    	var req = JSON.parse(xhttp.responseText);
+	     	document.querySelector(".showDes section div").innerHTML = "";
 	     	
+	     	var b =1;
 	     	for (var i = 0; i < req.length; i++) {
 	     		// console.log(req[i].sid);// for test
 	     		// console.log(req[i].description);// for test
+	     		
 	     		if(req[i].sid == a){
+	     			b++;
 	     			console.info(req[i].description);
+	     			console.log("!!!!!: ", infoFeed.style);
 	     			var el = document.createElement("div");
-	     			el.setAttribute("class", "showup");
 	     			el.textContent = req[i].description;
-	     			infoFeed.appendChild(el);
+	     			el.setAttribute("class", "showup");
+	     			
+	     			console.info("b: "+b);
+	     			el.style.animationDelay = 0.1*b+"s";
+	     			if(window.innerWidth>797){
+	     				infoFeed.appendChild(el);
+	     			}else{
+	     				var showDes = document.querySelector(".showDes");
+	     				document.querySelector(".showDes section b").textContent = title;
+	     				el.textContent ="> "+req[i].description;
+	     				showDes.style.display = "block";
+	     				document.querySelector(".gray").style.display = 'block';
+	     				document.querySelector(".showDes section div").appendChild(el);
+
+	     			}
 	     		}
 	     	};
 		}
-	  };
+	  } /*end of xhttp*/;
 	}
 	function getDataId(){
 		for (var i = 0; i < skillSet.length; i++) {
 			skillSet[i].addEventListener("click",function(){
+				var bars = document.querySelectorAll(".skillBar div");
+				
+				for (var j = 0; j < bars.length; j++) {
+					bars[j].setAttribute("class", "");
+				};
+				// alert(this.getAttribute("class"));
+				this.children[1].querySelector(".skillBar div").setAttribute("class", "clicked");
+				
+
+
+				console.info(this.children[0].textContent);
+				var title = this.children[0].textContent;
 				infoFeed.innerHTML   = "";
+				var skillTitle = document.createElement("h5");
+				skillTitle.textContent = title;
+				
+				skillTitle.style.fontWeight = "600";
+				skillTitle.style.textAlign = "center";
+				skillTitle.style.display = "block";
+				skillTitle.style.margin = "0.5em 0";
+				infoFeed.appendChild(skillTitle);
 				dataId = this.getAttribute("data-id");
 				console.log(dataId);//for test
-				loadDoc(dataId);
+				loadDoc(dataId, title);
 			})
 		};
+	}
+	function cross(){
+	  var cross = document.querySelector(".showDes section span");
+	  cross.addEventListener("click", function(){
+	  		document.querySelector(".showDes").style.display = "none";
+			document.querySelector(".gray").style.display = 'none';
+
+	  })
 	}
 	function resize(){
 		window.addEventListener("resize", function(){
 			document.querySelector(".container").style.width = window.innerWidth+"px";
+			var banners = document.querySelectorAll(".banner");
+
+				for (var i = 0; i < banners.length; i++) {
+					banners[i].style.width = document.querySelector(".container").innerWidth+"px";
+				};
+			
+			if(window.innerWidth<797){
+				fixizeBannaer();
+				
+			}
 		})
 	}
 	function fixizeBannaer(){
 		var bars = document.querySelectorAll(".banner");
+
 		var container = document.querySelector(".container");
-		
 		var a = bars[0].offsetTop;
 		var b = bars[1].offsetTop;
 		var c = bars[2].offsetTop;
 		container.addEventListener("scroll", function(){
-			var touchTop = container.scrollTop+32;
+			var touchTop = container.scrollTop;
 			console.log(touchTop);
 			console.log(bars[0].offsetTop);	
 			
@@ -61,24 +117,43 @@ window.addEventListener("load", function(){
 				if(touchTop > a){
 					console.log("offsetTop", bars[0].offsetTop);
 					bars[0].style.position = "fixed";
+					// bars[0].style.color = "red";
 					bars[0].style.zIndex = "1000";
+					bars[0].style.width = container.offsetWidth+"px";
 					bars[0].style.top="0";
+					bars[0].style.left="0";
+					bars[0].style.right="0";
+					bars[0].style.margin = "auto";
+					bars[0].style.borderBottom="2px solid black";
 				}else{
 					bars[0].style.position = "inherit";
-					// bars[0].style.zIndex = "inherit";
+					bars[0].style.borderBottom="none";
+					bars[0].style.width = "inherit";
+					
 					console.log("asdasd");
 				}
 
 				if((touchTop+bars[0].offsetTop+bars[0].offsetHeight) > b){
 					console.log("offsetTop", bars[1].offsetTop);
 					bars[1].style.position = "fixed";
+
 					bars[1].style.zIndex = "1111";
+					bars[1].style.width = container.offsetWidth+"px";
+
+					bars[1].style.left="0";
+					bars[1].style.right="0";
+					bars[1].style.margin = "auto";
+
+					bars[1].style.borderBottom="2px solid black";
 					bars[1].style.top=bars[0].offsetTop+bars[0].offsetHeight+"px";
 
 					
 				}else{
 					bars[1].style.position = "inherit";
 					bars[1].style.zIndex = "1111";
+					bars[1].style.borderBottom="none";
+					bars[1].style.width = "inherit";
+
 					// bars[1].style.top=bars[0].offsetTop+bars[0].offsetHeight+"px";
 				}
 				if((touchTop+bars[1].offsetTop+bars[1].offsetHeight) > c){
@@ -93,9 +168,98 @@ window.addEventListener("load", function(){
 
 		}) //end of listener
 	}
-	fixizeBannaer();
+	function audio(){
+		document.getElementById('audioElement').play();
+		document.getElementById('audioElement').volume = 0.2;
+		var b = true;
+		document.querySelector(".pause").addEventListener("click", function(){
+			if(b){
+				document.getElementById('audioElement').pause();
+				b = false;
+			}else{
+				document.getElementById('audioElement').play();
+				b = true;
+			}
+		})
+		var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+	  	var audioElement = document.getElementById('audioElement');
+		var audioSrc = audioCtx.createMediaElementSource(audioElement);
+		var analyser = audioCtx.createAnalyser();
+		// Bind our analyser to the media element source.
+		audioSrc.connect(analyser);
+		audioSrc.connect(audioCtx.destination);
+		//var frequencyData = new Uint8Array(analyser.frequencyBinCount);
+		var frequencyData = new Uint8Array(200);
+		var bars = document.querySelectorAll(".skillBar div");
+		var originalWidth = [];
+		for (var i = 0; i < bars.length; i++) {
+			originalWidth.push(parseInt(bars[i].style.width));
+		};
+
+		console.log(originalWidth);
+		function getSound(){
+			
+			requestAnimationFrame(getSound);
+			analyser.getByteFrequencyData(frequencyData);
+			var a = frequencyData[frequencyData.length-1]/20;
+			console.log(a);
+
+			for (var i = 0; i < bars.length; i++) {
+			var a = frequencyData[i]/5;
+
+				bars[i].style.width = originalWidth[i]+a+"%";
+			};
+
+		}
+		getSound();
+
+	}
+	
+	function scrollIt(){
+		var container = document.querySelector(".container");
+		container.addEventListener("scroll", function(){
+			
+			var intros = document.querySelectorAll(".intro");
+			var barss = document.querySelectorAll(".skillBar div");
+			if(container.scrollTop>350){
+				for (var i = 0; i < intros.length; i++) {
+					intros[i].style.display = "block";
+					intros[i].style.opacity = "1";
+				};
+				for (var i = 0; i < barss.length; i++) {
+					barss[i].style.display = "block";
+					barss[i].style.opacity = "1";
+				};
+			}
+
+			if(container.scrollTop<150){
+				function temp(){
+					for (var i = 0; i < 10; i++) {
+						
+					};
+				}
+				for (var i = 0; i < intros.length; i++) {
+					intros[i].style.opacity = "0";
+					temp();
+					intros[i].style.display = "none";
+				};
+				for (var i = 0; i < barss.length; i++) {
+					barss[i].style.opacity = "0";
+					temp();
+					barss[i].style.display = "none";
+				};
+			}
+
+		});
+	}
+	scrollIt();
+
+
 	resize();
 	getDataId();
+	audio();
+	cross()
+
 	
 
 })
